3e302030c1691aeacf75f6b02190f6ccbcdeb54a
IDIR ZAHIR <idirjob2023@gmail.com>
2023-11-27 00:22:12 -1200

    V1

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const infoDiv = document.querySelector(".infof");
-console.log(infoDiv);
+//const infoDiv = document.querySelector(".infof");
+//console.log(infoDiv);
+const from =document.querySelector(#from);
+const email =document.querySelector(#email);
+const password =document.querySelector(#password);
+const emailErro =document.querySelector(#emailErro);
+const passwordErreur =document.querySelector(#passwordErreur);
+
+console.log(from);
+console.log(email);
+
+alert(1);
